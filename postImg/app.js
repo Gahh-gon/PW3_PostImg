147dfa70134309dca4afa842914fee4aefa0bf01
Gahh-gon <77693978+Gahh-gon@users.noreply.github.com>
2022-12-01 20:29:26 -0300

    arrumando back

--- postImg/app.js
+++ postImg/app.js
@@ -3,17 +3,22 @@
 //testando e Aprendendo sobre o node e express
 
 const express = require('express');
+const routerController = require('./controller/routerController')
 
-
+ console.log('teste de funcionalidade');
 const app = express(); 
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const routerController = require('./controller/routerController')
+console.log(' passou no index ');
 
 app.use('/', routerController);
-
 app.listen(3000, ()=>{
     console.log('Servidor web rodando na url http://localhost:3000');
 })
+
+
+
+
+
--- postImg/controller/routerController.js
+++ postImg/controller/routerController.js
@@ -23,6 +23,9 @@ router.get("/listarCategoria", (req, res) =>{
     });
 
 
+    
+
+
 
     router.get('/listarCategoria/:cod_categoria',(req, res)=>{
 
@@ -44,6 +47,11 @@ router.get("/listarCategoria", (req, res) =>{
             );
     });
     
+
+   
+
+
+
 
     router.post('/inserirCategoria', (req, res)=>{
         let { nomeCategoria} = req.body;
@@ -67,7 +75,7 @@ router.get("/listarCategoria", (req, res) =>{
             }
         );
     });
-    
+
 
 
     router.put('/alterarCategoria', (req, res)=>{
@@ -97,6 +105,7 @@ router.get("/listarCategoria", (req, res) =>{
     });
     
 
+  
 
 
  router.delete('/deletarCategoria/:cod_categoria', (req, res)=>{
@@ -121,4 +130,107 @@ router.get("/listarCategoria", (req, res) =>{
             }
         );
     });
+
+
+  
+
     module.exports = router;
+
+
+
+
+//     const express = require('express');
+
+// /*CONFIGURAÇÃO DAS ROTAS DE CATEGORIA*/
+// const router = express.Router();
+
+// /* IMPORT DA MODEL DE CATEGORIA */
+// const modelCategoria = require('../model/CategoriaModel');
+
+// /* PARAMETROS DE ROTAS (QUALQUER VERBO):
+// 1 - NOME DA ROTA - REPRESENTADO POR UMA STRING
+// 2 - CALLBACK QUE TRATA REQUISIÇÃO (req) E RESPOSTA (res)
+// */
+// /*ROTAS DE CRUD DE CATEGORIAS:*/
+// router.get('/listarCategoria', (req, res)=>{
+
+//     // console.log('TESTE DE ROTA GET DE CATEGORIAS');
+//     // console.log('----A REQUISIÇÃO GET PASSOU PELA CATEGORIA CONTROLLER----');
+//     // res.send('----TESTE DE ROTA GET DE CATEGORIAS----');
+
+//     //LISTANDO OS DADOS SEM CRITÉRIOS
+//     modelCategoria.findAll()
+//         .then(
+//             (categorias)=>{
+//                 return res.status(200).json(categorias);
+//             }
+//         ).catch(
+//             (erro)=>{
+//                 return res.status(400).json({
+//                     erroStatus: true,
+//                     erroMessagem: 'Houve um erro ao selecionar os dados de categoria',
+//                     erroBancoDados: erro
+//                 });
+//             }
+//         );
+
+// });
+
+// //LISTANDO OS DADOS COM CRITÉRIOS
+// router.get('/listarCategoria/:id',(req, res)=>{
+
+//     let {id} = req.params;
+
+//     modelCategoria.findByPk(id)
+//         .then(
+//             (categoria)=>{
+//                 res.status(200).json(categoria);
+//             }
+//         ).catch(
+//             (erro)=>{
+//                 return res.status(400).json({
+//                     erroStatus: true,
+//                     erroMessagem: 'Houve um erro ao selecionar os dados de categoria',
+//                     erroBancoDados: erro
+//                 });
+//             }
+//         );
+
+// });
+
+// 
+
+// router.put('/alterarCategoria', (req, res)=>{
+
+//     // console.log('A REQUISIÇÃO PUT PASSOU PELA CATEGORIA CONTROLLER');
+//     // res.send('TESTE DE ROTA PUT DE CATEGORIAS');
+
+//     //RECEBENDO OS DADOS:
+//     let {id, nome_categoria} = req.body;
+
+//     //ALTERANDO OS DADOS:
+//     modelCategoria.update(
+//         {nome_categoria},
+//         {where:{id}}
+//     ).then( ()=>{
+
+//         return res.status(200).json({
+//             erroStatus: false,
+//             menssagemStatus: 'Categoria alterada com sucesso!'
+//         });
+
+//     }).catch(
+//         (erro)=>{
+//                     return res.status(400).json({
+//                         erroStatus: true,
+//                         erroMessagem: 'Houve um erro ao alterar a categoria',
+//                         erroBancoDados: erro
+//                     });
+//         }
+//     );
+
+// });
+
+
+
+// module.exports = router;
